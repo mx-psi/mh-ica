cb8206bfb172db28c90122bd785f1b7e5b7396dc
Pablo <pbaeyens31@gmail.com>
2018-06-10 13:36:27 +0200

    Calcula tabla de resultados

--- Impl/ICAmain.py
+++ Impl/ICAmain.py
@@ -1,5 +1,19 @@
 from ICA import *
 from cec2014 import cec2014
+import statistics
+import numpy as np
+
+def objetivo(i):
+  def f(x):
+    if "ncall" not in f.__dict__:
+      f.ncall = 0
+    else:
+      f.ncall += 1
+
+    if not x.flags["C_CONTIGUOUS"]:
+      x = np.ascontiguousarray(x)
+    return cec2014.cec14(x,i)
+  return f
 
 params = {
   "ncountries": 80,
@@ -7,21 +21,29 @@ params = {
   "decades": 2000,
   "initial_revolution_rate": 0.3,
   "assimilation_coef": 2,
-  "assimilation_angle_coef": 0.5,
   "zeta": 0.02,
   "damp_ratio": 0.99,
-  "stop_if_just_one_empire": True,
+  "stop_if_just_one_empire": False,
   "uniting_threshold": 0.02,
   "zarib":1.05, # Zarib is used to prevent the weakest empire to have a probability of zero
   "alpha": 0.1  # importance of the mean minimun compared to the global minimum. Must be << 1
 }
 
+DIM = 30
 problem_domain = {
-  "dim": 30,
+  "dim": DIM,
   "lower_bound": -100,
-  "upper_bound": 100  # tuple including lower and upper bounds
+  "upper_bound": 100,  # tuple including lower and upper bounds
+  "max_evals": 10000*DIM # 10000*dim
 }
 
 if __name__ == "__main__":
-  objetivo = lambda x: cec2014.cec14(x,19)
-  ICA(objetivo, params, problem_domain)
+  print("F","Media","Desviación estándar", sep=", ")
+  for i in range(1,21):
+    a = []
+    for _ in range(25):
+      best = ICA(objetivo(i), params, problem_domain)
+      a.append(objetivo(i)(best) - 100*i)
+    m = statistics.mean(a)
+    s = statistics.stdev(a)
+    print(i,m,s, sep = ", ")
